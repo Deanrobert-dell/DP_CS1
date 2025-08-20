48c04eae7a68bca3db76b05e9be1418af21c92c2
Deanrobert-dell <dean.peterson@ucas-edu.net>
2025-08-20 10:46:31 -0600

    time

--- notes/Practices/hello_world.py
+++ notes/Practices/hello_world.py
@@ -1,6 +1,12 @@
 #DP 2nd hello world
+import time
 user = input("who are you ")
 if user == "dean":
     print ("welcome back",user)
+    time.sleep (1)
+    print ("logging back in")
 else:
-    print ("welcome", user)
+    print ("welcome new user", user)
+    time.sleep (1)
+    print ("please login")
+
