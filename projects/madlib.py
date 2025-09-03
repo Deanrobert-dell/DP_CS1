ff286a77de776f10c7e73fccbbe8d98ffdcf49cc
Deanrobert-dell <dean.peterson2ucas-edu.net>
2025-09-03 10:49:35 -0600

    madlib1

---
+++ projects/madlib.py
@@ -0,0 +1,12 @@
+nounPlace = input ("input a place/location: ")
+noun = input ("input a noun: ")
+noun2 = input ("input another noun: ")
+adjective = input ("input an adjective: ")
+verb = input ("input a verb: ")
+name = input ("input a name: ")
+adjective2 = input ("input another adjective: ")
+noun3 = input ("input another noun: ")
+
+madlib = ("one day " + (name) + " was eating a " + (adjective) + " " + (noun) + " in " + (nounPlace) + ", " + (name) + " then realized he needed a "+ (noun3) + " he " + (verb) + " to the store and got a " + (adjective2) + " " + (noun2) + " and went back to " + (nounPlace) + ".")
+
+print (madlib)
