72a355f1979b5d73d4ef30c00fa755bd1266ede1
Deanrobert-dell <dean.peterson2ucas-edu.net>
2025-09-04 10:50:35 -0600

    project5

---
+++ notes/fox.py
@@ -0,0 +1,11 @@
+
+sentence= ("the black brick")
+word = "black"
+
+
+print(sentence.replace(word, 'yellow'))
+
+age= input('whats your age')
+name= input ("whats your name")
+
+print ("hello {} it is cool that you are {}".format(name, age))
---
+++ projects/name.py
@@ -0,0 +1,2 @@
+name=input("What is your name: ")
+pnumber=input ("what is your phone number: ")
