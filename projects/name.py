bd1bb4c9039c943995dd33a23264b29bf8a1418d
Deanrobert-dell <dean.peterson2ucas-edu.net>
2025-09-05 10:50:58 -0600

    sync111

--- projects/name.py
+++ projects/name.py
@@ -1,2 +1,16 @@
-name=input("What is your name: ")
-pnumber=input ("what is your phone number: ")
+name = input("What is your name: ").strip().title()
+pnumber = input ("what is your phone number: ").strip()
+gpa = input("what is your gpa: ").strip()
+word = "-"
+name_nospaces = name.replace(" ", "")
+print(name_nospaces)
+print(pnumber.replace(word, ' '))
+
+
+
+
+if (gpa).isnumeric():
+    print (gpa)
+else: 
+    print ("put in numerical data for gpa")
+
