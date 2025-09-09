de8b150ed51843e27996c8d86c51b62a79e055bb
Deanrobert-dell <dean.peterson2ucas-edu.net>
2025-09-09 10:51:10 -0600

    calc2

---
+++ projects/calculator.py
@@ -0,0 +1,21 @@
+#DP 2nd calculator
+inp1 = input  ("input one number to be calculated: ")
+inp2= input ("Input a second number: ")
+add =  float(inp1)  + float(inp2)
+sub = float(inp1)  - float(inp2)
+mult = float(inp1)  * float(inp2)
+div = float(inp1)  / float(inp2)
+intdiv = float(inp1)  // float(inp2)
+mod = float(inp1)  % float(inp2)
+exp = float(inp1)  ** float(inp2)
+
+print (round(add, 2))
+print (round (sub, 2))
+print (round (mult, 2))
+print (div)
+print (intdiv)
+print (mod)
+print (exp)
+
+
+
