0500d5a5fd39098de31917b3e44c71a6ea183806
Deanrobert-dell <dean.peterson@ucas-edu.net>
2025-09-16 17:03:11 -0600

    Update crew.py

--- projects/crew.py
+++ projects/crew.py
@@ -1,10 +1,24 @@
-#DP 2nd crew shares
-money = input("what was the money earned in dollars : ")
-members = input ("how maney crew members are their (besides captain and firstmate): ")
-#how much money was given to crewmates for evening
-mofoev = 500 * float(members)
-share = 7 + 3 + float(members)
+# DP 2nd crew shares
 
-print ("captain gets", 7 * share )
-print ("firstmate gets", 3 * share)
-print ("crew still needs", share *  )
+# Ask inputs
+money = float(input("What was the money earned in dollars: "))
+members = int(input("How many crew members are there (besides captain and first mate): "))
+
+# Total shares: 
+total_shares = 7 + 3 + members
+
+# money in one share
+share = money / total_shares
+
+# money owed
+captain_Money = 7 * share
+firstmate = 3 * share
+crew_Total = members * share
+
+#outputs
+print("Captain gets", round(captain_Money, 2))
+print("First mate gets", round(firstmate, 2))
+print("crew still needs ", round(crew_Total, 2))
+print ("each crew member gets ", round(crew_Total / members, 2))
+
+
