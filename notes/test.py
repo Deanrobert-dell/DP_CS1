91530d9e5d8d3cf456d6437e6efd6becda8c7f5c
Deanrobert-dell <dean.peterson@ucas-edu.net>
2025-09-18 10:51:16 -0600

    notesnew

--- notes/test.py
+++ notes/test.py
@@ -1,6 +1,7 @@
 # DP 2nd Inputs & Outputs
-
+import time
 import random
+import datetime as date
 
 print (random.random())
 print (random.randint(1,20))
@@ -10,6 +11,15 @@ while number == 2:
     print(int("4") + 1)
 
 print ("new seat 9/17")
+currenttime = time.time()
+readable_time = time.ctime(currenttime)
+now= date.datetime.novvvw()
+hour=now.strftime("%H ""%M")
+#month %m or %B, minute %M, second %S, day %D, year %Y or %y 
 
+print (f"Time in seconds since 1970: {currenttime}")
+print (f"current time: {readable_time}")
+print (f"current time: {now}")
+print (F"mil time is {hour}")
 
 
