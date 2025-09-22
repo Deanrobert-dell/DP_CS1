721760ecea74b4a64d14e47906c4fe19402e9db4
Deanrobert-dell <dean.peterson@ucas-edu.net>
2025-09-22 10:20:02 -0600

    updatesignin

--- projects/signin.py
+++ projects/signin.py
@@ -3,7 +3,7 @@ username = input ("enter username: ")
 login = input ("enter password: ")
 if username == ("dean") and login == ("12345"):
     print ("signing in...")
-elif login != ("12345"):
-    print ("wrong password")
+elif username == ("dean") and login != ("12345"):
+    print ("wrong password... retry")
 else:
     print ("login failed")
