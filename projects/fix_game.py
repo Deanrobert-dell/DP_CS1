47221cd97c180ad501623382c3b0bd6b18b70c64
Deanrobert-dell <dean.peterson@ucas-edu.net>
2025-09-26 10:32:23 -0600

    errorfixed

--- projects/fix_game.py
+++ projects/fix_game.py
@@ -14,20 +14,16 @@ def start_game():
         if attempts >= max_attempts:
             print(f"Sorry, you've used all {max_attempts} attempts. The number was {number_to_guess}.")
             game_over = True
-        elif guess == number_to_guess: #There cannot be 2 if conditionals, this is wrong because both will be checked, (syntax)
+        elif guess == number_to_guess: #There cannot be 2 if conditionals, this is wrong because both will be checked, (logic)
             print("Congratulations! You've guessed the number!")
             game_over = True
         elif guess > number_to_guess:
             print("Too high! Try again.")
-            attempts=attempts+1 #adds one to the attempts
-        elif guess < number_to_guess:
+            attempts=attempts+1 #adds one to the attempts this is part of my first comment
+        else: #pointless elif statement, this was simply not needed (logic)
             print("Too low! Try again.") 
-            attempts=attempts+1 #adds one to the attempts
+            attempts=attempts+1 #adds one to the attempts this is part of my first comment
         continue
     print("Game Over. Thanks for playing!")
 start_game()
 
-
-
-
-#qwertyuiopasdfghjklzxcvbnm false true \ / ! >< + =() #
