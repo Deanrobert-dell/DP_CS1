81007db8535095ef5a65f03c21298575e3f63563
Deanrobert-dell <dean.peterson@ucas-edu.net>
2025-09-26 10:44:17 -0600

    errorsjdoiajd

--- projects/fix_game.py
+++ projects/fix_game.py
@@ -10,7 +10,7 @@ def start_game():
     attempts = 0 #originally this stayed at 0 forever, that means you would never run out of guesses (logic)
     game_over = False
     while not game_over:
-        guess = float(input("Enter your guess: ")) #wrong type of data, the > operator doesnt work with integers (runtime)
+        guess = float(input("Enter your guess: ")) #wrong type of data, the > operator doesnt work with integers and strings (runtime)
         if attempts >= max_attempts:
             print(f"Sorry, you've used all {max_attempts} attempts. The number was {number_to_guess}.")
             game_over = True
@@ -23,7 +23,7 @@ def start_game():
         else: #pointless elif statement, this was simply not needed (logic)
             print("Too low! Try again.") 
             attempts=attempts+1 #adds one to the attempts this is part of my first comment
-        continue
     print("Game Over. Thanks for playing!")
 start_game()
 
+#continue didn't do anything, this didnt stop code but it was useless (logic)
