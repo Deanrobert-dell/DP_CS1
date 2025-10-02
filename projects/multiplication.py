214f777059255d0efc52f49b562f32fd034ff426
Deanrobert-dell <dean.peterson@ucas-edu.net>
2025-10-02 16:46:32 -0600

    Update multiplication.py

--- projects/multiplication.py
+++ projects/multiplication.py
@@ -1,12 +1,7 @@
+table = [1,2,3,4,5,6,7,8,9,10,11,12]
 
-
-
-
-
-table = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,]
-
-for item in range(1,13):
-    print (item )
-
-for i in table:
-    print (item*table )
+for item in table:
+    row = ""
+    for i in table:
+        row = row + str(item * i) + ' '  # concatinates spaces betwen numbers
+    print(row)  # prints the row that i created
