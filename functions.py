2221f78cf608a21191f9ae1fb0c976eddd31bfa4
Deanrobert-del <dean.peterson@ucas-edu.net>
2025-10-20 10:51:21 -0600

    mskdsanbfjdhnoisgfd

---
+++ functions.py
@@ -0,0 +1,27 @@
+def add():
+    print (f"5 + 5 = {5+5}")
+add()
+
+def add(x,y):
+    print (f"{x} + {y} = {x+y}")
+
+
+
+add(5,5)
+
+add(3.1,88)
+
+
+def add(x,y):
+    return x+y
+
+
+num = 0
+while num < add (9999,5):
+    print ("duck")
+    num += 1
+print("goose")
+print(f"resuolt is: {add (44,67)}")
+
+add(5,5)
+add(3.1,88)
