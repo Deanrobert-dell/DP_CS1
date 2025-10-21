8ff7f7ace93307ab136e17fd7f13d31f68826542
Deanrobert-del <dean.peterson@ucas-edu.net>
2025-10-21 10:52:23 -0600

    nvjfdsonhlkdsf

--- functions.py
+++ functions.py
@@ -11,17 +11,39 @@ add(5,5)
 
 add(3.1,88)
 
+playerhp = 100
 
 def add(x,y):
     return x+y
 
 
 num = 0
-while num < add (9999,5):
+while num < add (2,2):
     print ("duck")
     num += 1
 print("goose")
-print(f"resuolt is: {add (44,67)}")
+print(f"result is: {add (1,2)}")
 
 add(5,5)
 add(3.1,88)
+
+
+
+
+
+
+
+def initials(name):
+    names = name.split(" ")
+    initial = " "
+    for name in names:
+        initial += name [0]
+    return initial
+
+print (f"deans initials are: {initials("dean peterson")}")
+
+
+
+
+def attack (dmg):
+    return playerhp - dmg
---
+++ gamble.py
@@ -0,0 +1,16 @@
+import random
+print("its gamblin time")
+money = 500
+
+while money >=0:
+    print("you have",money,"money " )
+    choice = input("want to coin flip 50 percent chance (1) want to do lottery, 5 percent chance (2) or want to quit (3)")
+    if choice == 1:
+        user_move = (rock)
+    elif user_input == 2:
+        user_move = (paper)
+    elif user_input == 3:
+        user_move = (scissor)
+    elif user_input == 4:
+        print ("quiting")
+        break
