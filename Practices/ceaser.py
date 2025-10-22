c07715f4ed7975d0299717edba2301dd1a992ab4
Deanrobert-del <dean.peterson@ucas-edu.net>
2025-10-22 10:50:53 -0600

    heufagiyujdshdslkgfds

---
+++ Practices/ceaser.py
@@ -0,0 +1,55 @@
+#DP 2nd ceasar code
+
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+code = input("what is your password: ")
+
+a = 0
+b = 1
+c = 2
+d = 3
+e = 4
+f = 5
+g = 6
+h = 7
+i = 8
+j = 9
+k = 10
+l = 11
+m = 12
+n = 13
+o = 14
+p = 15
+q = 16
+r = 17
+s = 18
+t = 19
+u = 20
+v = 21
+w = 22
+x = 23
+y = 24
+z = 25
+
+
+question = input("do you want to encode (1) or decode (2)")
+
+
+for index, value in enumerate(letters):
+    print(f"Index: {index}, Value: {value}")
+
+
+
+
+
+
+for x in code:
+    if x in letters:
+        char = True
+        break 
+
+if char:
+    print("you have a special chrachter✓")
+    strength += 1
+else: 
+    print ("need special charachter")
+
