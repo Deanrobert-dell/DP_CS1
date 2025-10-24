cd7bf94c01d328fd9a42873c0a7a2097dd1f4057
Deanrobert-del <dean.peterson@ucas-edu.net>
2025-10-24 10:51:11 -0600

    saffffffffffasdsadsa

---
+++ projects/combat.py
@@ -0,0 +1,25 @@
+#DP 2nd combat code
+import random 
+
+hp = (50)
+attack = (10)
+defense = (2)
+
+
+randmove = random.randint(1,2)
+randmove1 = random.randint(1,3)
+randmove2 = random.randint(1,5)
+randmove3= random.randint(1,10)
+print (randmove)
+
+name = input('What is your name: ')
+type = input('what is your class: Warrior (1), Mage (2), normal human (3)')
+
+
+if type == "1":
+    print (name,"your stats as a Warrior are HP: ", hp - randmove2,", attack: ", attack + randmove, "Defense: ", defense)
+
+elif type == "2":
+    print (name,"your stats as a Mage are HP: ", hp + randmove3,", attack: ", attack - randmove, "Defense: ", defense + randmove2)
+elif type == "3":
+    print (name,"your stats as a HUMAN are HP: ", hp - randmove3,", attack: ", attack - randmove3, "Defense: ", defense)
