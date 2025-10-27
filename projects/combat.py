5da7feb05b5aa94233aa61c6652f09a3d10a5ecd
Deanrobert-del <dean.peterson@ucas-edu.net>
2025-10-27 10:51:02 -0600

    bbbbbbbbbbbbbbbbldsd

--- projects/combat.py
+++ projects/combat.py
@@ -18,8 +18,18 @@ type = input('what is your class: Warrior (1), Mage (2), normal human (3)')
 
 if type == "1":
     print (name,"your stats as a Warrior are HP: ", hp - randmove2,", attack: ", attack + randmove, "Defense: ", defense)
-
+    attacktype = "axe chop"
 elif type == "2":
     print (name,"your stats as a Mage are HP: ", hp + randmove3,", attack: ", attack - randmove, "Defense: ", defense + randmove2)
+    attacktype = "fire ball"
 elif type == "3":
-    print (name,"your stats as a HUMAN are HP: ", hp - randmove3,", attack: ", attack - randmove3, "Defense: ", defense)
+    print (name,"your stats as a Human are HP: ", hp - randmove3,", attack: ", attack - randmove3, "Defense: ", defense)
+    attacktype = "slap"
+def monster():
+    m_damage = random.randint(2,14)
+    m_health = random.randint (45,70)
+monster()
+
+
+atrackinp = input("press one to",attacktype,"enemy" )
+
