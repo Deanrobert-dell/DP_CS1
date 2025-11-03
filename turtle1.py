77ebcbddee8369103f13b08f1f55302808eb32f4
Deanrobert-del <dean.peterson@ucas-edu.net>
2025-11-03 10:52:35 -0700

    afkjhrsgbiufdsghldiu;gd

--- turtle1.py
+++ turtle1.py
@@ -3,6 +3,7 @@ import random
 
 space = random.randint(1,4)
 
+gap = 40
 
 
 
@@ -34,50 +35,203 @@ line7 = turtle.Turtle()
 line8 = turtle.Turtle()
 line9 = turtle.Turtle()
 
+
+up = turtle.Turtle()
+up1 = turtle.Turtle()
+up2 = turtle.Turtle()
+up3 = turtle.Turtle()
+up4 = turtle.Turtle()
+up5 = turtle.Turtle()
+up6 = turtle.Turtle()
+up7 = turtle.Turtle()
+up8 = turtle.Turtle()
+up9 = turtle.Turtle()
+
+up.penup()
+up.setpos(-500,50)
+up.pendown
+up.left
+up.pendown()
+up.forward(random.randint(100,300))
+up.penup()
+up.forward(40)
+up.pendown()
+up.forward(random.randint(100,300))
+up.penup()
+up.forward(40)
+up.pendown()
+up.forward(random.randint(100,300))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 line.penup()
 line.setpos(-500,50)
 line.pendown()
-line.forward(460)
+line.forward(random.randint(100,300))
+line.penup()
+line.forward(40)
+line.pendown()
+line.forward(random.randint(100,300))
+line.penup()
+line.forward(40)
+line.pendown()
+line.forward(random.randint(100,300))
+    
+
 
 line1.penup()
 line1.setpos(-500,100)
 line1.pendown()
-line1.forward(460)
+line1.forward(random.randint(100,300))
+line1.penup()
+line1.forward(40)
+line1.pendown()
+line1.forward(random.randint(100,300))
+line1.penup()
+line1.forward(40)
+line1.pendown()
+line1.forward(random.randint(100,300))
 
 line2.penup()
 line2.setpos(-500,150)
 line2.pendown()
-line2.forward(460)
+line2.forward(random.randint(100,300))
+line2.penup()
+line2.forward(40)
+line2.pendown()
+line2.forward(random.randint(100,300))
+line2.penup()
+line2.forward(40)
+line2.pendown()
+line2.forward(random.randint(100,300))
 
 line3.penup()
 line3.setpos(-500,200)
 line3.pendown()
-line3.forward(460)
+line3.forward(random.randint(100,300))
+line3.penup()
+line3.forward(40)
+line3.pendown()
+line3.forward(random.randint(100,300))
+line3.penup()
+line3.forward(40)
+line3.pendown()
+line3.forward(random.randint(100,300))
 
 line4.penup()
 line4.setpos(-500,250)
 line4.pendown()
-line4.forward(460)
+line4.forward(random.randint(100,300))
+line4.penup()
+line4.forward(40)
+line4.pendown()
+line4.forward(random.randint(100,300))
+line4.penup()
+line4.forward(40)
+line4.pendown()
+line4.forward(random.randint(100,300))
 
 line5.penup()
 line5.setpos(-500,300)
 line5.pendown()
-line5.forward(460)
+line5.forward(random.randint(100,300))
+line5.penup()
+line5.forward(40)
+line5.pendown()
+line5.forward(random.randint(100,300))
+line5.penup()
+line5.forward(40)
+line5.pendown()
+line5.forward(random.randint(100,300))
 
 line6.penup()
 line6.setpos(-500,350)
 line6.pendown()
-line6.forward(460)
+line6.forward(random.randint(100,300))
+line6.penup()
+line6.forward(40)
+line6.pendown()
+line6.forward(random.randint(100,300))
+line6.penup()
+line6.forward(40)
+line6.pendown()
+line6.forward(random.randint(100,300))
 
 line7.penup()
 line7.setpos(-500,400)
 line7.pendown()
-line7.forward(460)
+line7.forward(random.randint(100,300))
+line7.penup()
+line7.forward(40)
+line7.pendown()
+line7.forward(random.randint(100,300))
+line7.penup()
+line7.forward(40)
+line7.pendown()
+line7.forward(random.randint(100,300))
 
 line8.penup()
 line8.setpos(-500,450)
 line8.pendown()
-line8.forward(460)
+line8.forward(random.randint(100,300))
+line8.penup()
+line8.forward(40)
+line8.pendown()
+line8.forward(random.randint(100,300))
+line8.penup()
+line8.forward(40)
+line8.pendown()
+line8.forward(random.randint(100,300))
 
 
 
