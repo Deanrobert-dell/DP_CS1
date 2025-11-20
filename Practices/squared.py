0facb4e923f7a3519de95b716a566c639cc9222e
Deanrobert-dell <dean.peterson@ucas-edu.net>
2025-11-20 10:51:02 -0700

    sedgsrg

--- Practices/squared.py
+++ Practices/squared.py
@@ -1,7 +1,7 @@
 #DP 2nd Squared Numbers
 
 #create list of numbers
-nums = [3, 7, 12, 25, 30, 45, 50, 65, 70, 85, 90, 105, 110, 125, 130, 145, 150, 165, 170, 185, 190, 205, 210, 225, 230, 245, 250, 265, 270, 285]
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
 
 #function that i will call that basically sets what squared is
 squared = list(map(lambda num: num*num, nums))
