094cfa218a0cd221886a218d763b5f73bad13bf2
Deanrobert-dell <dean.peterson@ucas-edu.net>
2025-11-20 10:13:39 -0700

    ftdshjydewgtsershafdg

--- Practices/squared.py
+++ Practices/squared.py
@@ -1,16 +1,11 @@
 #DP 2nd Squared Numbers
 
+#create list of numbers
 nums = [3, 7, 12, 25, 30, 45, 50, 65, 70, 85, 90, 105, 110, 125, 130, 145, 150, 165, 170, 185, 190, 205, 210, 225, 230, 245, 250, 265, 270, 285]
 
+#function that i will call that basically sets what squared is
+squared = list(map(lambda num: num*num, nums))
 
-divided = []
-"""for num in numbers:
-    divided.append(num/2)"""
-
-"""def divide(number):
-    return number/2"""
-
-divided = list(map(lambda num: num/2, numbers))
-
-for i, num in enumerate(numbers):
-    print(f"{num} divided by 2 is {divided[i]}")
+#for loop that goes over list
+for i, num in enumerate(nums):
+    print(f"original: {num} squared is {squared[i]}") #print the numbers before and after squared
