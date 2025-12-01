88ff922f259586df5c241b3ea0486d558e38973c
Deanrobert-dell <dean.peterson@ucas-edu.net>
2025-12-01 10:51:38 -0700

    hjgfdhjdsjmhfd,jhnf

---
+++ FINAL/pseudo.py
@@ -0,0 +1,4 @@
+#print statement welcoming player
+#print backstory explaining where you are
+#ask user if they wish to play the game now that they know what happens
+#function for each room, leading paths to other rooms, within the room functions thats where the monsters and paths are
---
+++ FINAL/trutleart.py
@@ -0,0 +1,9 @@
+import turtle
+def turtlemaze():
+    box= turtle.Turtle()
+    box.speed(0)
+    box.hideturtle()
+    box.setpos(-200,0)
+    box.penup()
+    
+turtlemaze()
