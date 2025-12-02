9f90ee0c546b121e1be5fb4acf5f32669b401528
Deanrobert-dell <dean.peterson@ucas-edu.net>
2025-12-02 10:51:39 -0700

    eyteofra

--- FINAL/pseudo.py
+++ FINAL/pseudo.py
@@ -1,4 +1,22 @@
+#variables: health damage luck 
+#variable for secret room, changes if room is entered so room cant be entered again
+#dictionary for "inventory"
+
+#function for each room, change boolean if entered to make sure interactions cant happen twice
+
+#example function for room:
+#welcoming to room
+#print description of room
+#if monster first thing is fight or run
+#if no monster describe any chests or suspicious walls
+
+
 #print statement welcoming player
 #print backstory explaining where you are
 #ask user if they wish to play the game now that they know what happens
 #function for each room, leading paths to other rooms, within the room functions thats where the monsters and paths are
+
+
+
+
+
