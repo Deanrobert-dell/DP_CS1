ae1fd1c6f7dbd3b30f4d091869713d5bda98b472
Deanrobert-dell <dean.peterson@ucas-edu.net>
2025-12-03 10:54:00 -0700

    6yterju654ytjrthdyyrydkt

--- FINAL/pseudo.py
+++ FINAL/pseudo.py
@@ -9,12 +9,56 @@
 #print description of room
 #if monster first thing is fight or run
 #if no monster describe any chests or suspicious walls
+#suspicious wall for secret room
+#to salve the problem of being able to pick up items multiple times, ill make an if statement checking if item is in dictionary if not then it will present item or if yes then it wont, for moinsters, ill either make them drop an item that can be checked, or have a variable that can be changed
+
+
+#ROOM1
+
+#just entered pyramid, entrance closed behind you
+#there is a path to left and forward
+#user input where to go
+
+#ROOM2
+#there is a chest in the corner, it has a combination lock choose 1 or 2 to open
+#if wrong imput take damage
+#if right find cheesy mummy toe, eat to get +2 health
+
+
+#ROOM3
+#description
+#do you wish to investigate suspicious wall, or move on forward or to thr right
+
+
+#ROOM4
+
+
+
+#ROOM5
+
+
+
+#ROOM6
+
+
+
+#ROOM7
+
+
+
+#secret ROOM
+
+
+
+#final ROOM
+
 
 
 #print statement welcoming player
 #print backstory explaining where you are
 #ask user if they wish to play the game now that they know what happens
 #function for each room, leading paths to other rooms, within the room functions thats where the monsters and paths are
+
 
 
 
