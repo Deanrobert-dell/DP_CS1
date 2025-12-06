1552f7caff07e983f14cb4ddf5b5a5153e255a62
Deanrobert-dell <dean.peterson@ucas-edu.net>
2025-12-05 17:51:29 -0700

    Improve comments structure in pseudo.py
    Refactor comments for clarity and organization in pseudo.py

--- FINAL/pseudo.py
+++ FINAL/pseudo.py
@@ -1,74 +1,110 @@
-#variables: health damage defense
-#variable for secret room, changes if room is entered so room cant be entered again
-#dictionary for "inventory"
+# VARIABLES
 
-#function for each room, change boolean if entered to make sure interactions cant happen twice
+# variables: health, damage, defense
+# dictionary for inventory
+# boolean variables for each room so interactions can't happen twice
+# variable for secret room, changes if room is entered so room cant be entered again
+# enemy state variables like mummy, scarab, boss
 
-#example function for room:
-#welcoming to room
-#print description of room
-#if monster first thing is fight or run
-#if no monster describe any chests or suspicious walls
-#suspicious wall for secret room
-#to salve the problem of being able to pick up items multiple times, ill make an if statement checking if item is in dictionary if not then it will present item or if yes then it wont, for moinsters, ill either make them drop an item that can be checked, or have a variable that can be changed
+#game functions
 
+# startGame()
+#   print statement welcoming player
+#   print backstory explaining where you are (in pyramid 
+#   ask user if they wish to play the game now that they know what happens
+#   if yes go to room1
+#   if no exit game (break)
 
-#ROOM1
+# combat()    health and damage
+#     player chooses attack or defense
+#   update player health and enemy health based on damage and defense
+#   loop keeps going until one reaches 0
+#   return win or lose 
 
-#just entered pyramid, entrance closed behind you
-#there is a path to left and forward
-#user input where to go
+# pickupitem()
+#   if item not in inventory then add it
+#   if item already in dictionary then dont add (prevents repeteable pickup)
 
-#ROOM2
-#there is a chest in the corner, it has a combination lock choose 1 or 2 to open
-#if wrong imput take damage
-#if right find cheesy mummy toe, eat to get +2 health
+# changestats() function to change statistics
+#   increase health or damage or defense depending on itemm that are pick up
 
+# restartghame()
+#   reset stats
+#   reset all booleans
+#   reset enemy defeated variables
+#   send player back to start game function
+#basically start over
 
-#ROOM3
-#description
-#do you wish to investigate suspicious wall, or move on forward or to thr right
-#leads to secret room
-#nothing else in room
+#room functions 
 
+# function for each room, change boolean if entered to make sure interactions cant happen twice
+# example function steps:
+#   welcoming to room
+#   print description of room
+#   if monster present first thing is fight or run (i might not add run as an option
+#   if no monster describe chest for loot or suspicious wallcfor secret room
+#   suspicious wall leads to secret room
+#   to stop picking up items twice check inventory first whichis dictionary
+#   for monsters use a defeated variable so fight only happens once
 
-#ROOM4
-# as player enters, they are attacked by a mummy fo -1 health
-#if player dies allow them to restart
-#if player wins recieve khopesh sword for +2 damage
+# ROOM1
+# just entered pyramid, entrance closed behind you
+# there is a path to left and forward
+# user input where to go
+# leads to room2 or room3
 
+# ROOM2
+# there is a chest in the corner it has a combination lock choose 1 or 2 to open (will make random so cant repeat on playthrough)
+# if wrong input take damage
+# if right find cheesy mummy toe, eat to get +2 health
+# check if item already in inventory
 
+# ROOM3
+# description of the room
+# do you wish to investigate suspicious wall, or move on forward or to the right
+# suspicious wall leads to secret room
+# nothing else in room
 
-#ROOM5
-# as player enters there is a well, sacrifice 1 health to gain random item
-#leads to  3 2 and 7
+# SECRET ROOM
+# only allow entry if sroom var is False
+# once entered set secret room variable  to True
+# special interaction or item here (tbd
+# leads back to room3 or room5
 
+# ROOM4
+# as player enters, they are attackeed by a mummy for -1 health
+# if player dies allow them to restart
+# if player wins recieve khopesh sword for +2 damage
+# check mummy var so fight doesn't happen again
 
-#ROOM6
-# as they enter enter fight with giant scarab beetle
-# if they win they can equip scarab shell sheild or 2 defense
+# ROOM5
+# as player enters there is a well
+# sacrifice 1 health to gain random item
+# leads to rooms 3, 2, and 7
 
+# ROOM6
+# as they enter they fightt giant scarab beetle
+# if player wins they can equip scarab shell shield for +2 defense
+# check scarab var so it doesn't fight again
 
-#ROOM7
-#in the room there are 3 alters each one with an item for either health damage or defense, the player thinks they can get all three, but once they get one the room crumbles and falls and they are forced into the boss room
+# ROOM7
+# in the room there are 3 alters each one with an item for either health damage or defense
+# the player thinks they can get adll three
+# but once they get one the room is destroyed
+# they are forced into the boss room (finalRoom)
 
+# FINAL ROOM
+# contains final boss combat
+# if win print victory message 
+# if lose ask if they wsant to restartGame() and call them loser
 
+# MAIN GAME LOOP
 
-#secret ROOM
-
-
-
-#final ROOM
-
-
-
-#print statement welcoming player
-#print backstory explaining where you are
-#ask user if they wish to play the game now that they know what happens
-#function for each room, leading paths to other rooms, within the room functions thats where the monsters and paths are
-
-
-
-
-
-
+# startGame()
+# while playing:
+# room1()
+# player go to other rooms based on their chioices
+# combat happens inside room functions when needed in specififc rooms
+# finalRoom() ends the game plus boss
+#     break once finished
+#can loop when dead so replayaable 
